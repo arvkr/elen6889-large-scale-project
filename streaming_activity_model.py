c02ee1a36f708b74d850365f3795794c9ea28f17
Arvind Kanesan Rathna <35028458+arvkr@users.noreply.github.com>
2022-05-06 01:45:33 -0400

    Add files via upload

--- streaming_activity_model.py
+++ streaming_activity_model.py
@@ -3,17 +3,19 @@ from pyspark.sql.functions import from_json, col, split
 from pyspark.sql import functions as F
 
 userSchema = StructType() \
-            .add('date', 'date') \
-            .add('time', 'timestamp') \
-            .add('username', 'string') \
-            .add('wrist', 'integer') \
+            .add('acceleration_x1', 'float') \
+            .add('acceleration_x2', 'float') \
+            .add('acceleration_x3', 'float') \
+            .add('acceleration_x4', 'float') \
+            .add('acceleration_x5', 'float') \
+            .add('acceleration_x6', 'float') \
+            .add('acceleration_x7', 'float') \
+            .add('acceleration_x8', 'float') \
+            .add('acceleration_x9', 'float') \
+            .add('acceleration_x10', 'float') \
+            .add('acceleration_x11', 'float') \
+            .add('acceleration_x12', 'float') \
             .add('activity', 'integer') \
-            .add('acceleration_x', 'float') \
-            .add('acceleration_y', 'float') \
-            .add('acceleration_z', 'float') \
-            .add('gyro_x', 'float') \
-            .add('gyro_y', 'float') \
-            .add('gyro_z', 'float') \
             .add('timestamp', 'timestamp')
 
 
@@ -25,20 +27,24 @@ df = spark \
   .load() \
   .select(from_json(col("value").cast("string"), userSchema).alias('parsed_value'))
 
-df2 = df.withColumn("date", split(df["parsed_value"].cast("string"), ", ").getItem(0))\
-        .withColumn("time", split(df["parsed_value"].cast("string"), ", ").getItem(1))\
-        .withColumn("u", split(df["parsed_value"].cast("string"), ", ").getItem(2))\
-        .withColumn("w", split(df["parsed_value"].cast("string"), ", ").getItem(3))\
-        .withColumn("a", split(df["parsed_value"].cast("string"), ", ").getItem(4))\
-        .withColumn("ax", split(df["parsed_value"].cast("string"), ", ").getItem(5))\
-        .withColumn("ay", split(df["parsed_value"].cast("string"), ", ").getItem(6))\
-        .withColumn("az", split(df["parsed_value"].cast("string"), ", ").getItem(7))\
-        .withColumn("gx", split(df["parsed_value"].cast("string"), ", ").getItem(8))\
-        .withColumn("gy", split(df["parsed_value"].cast("string"), ", ").getItem(9))\
-        .withColumn("gz", split(df["parsed_value"].cast("string"), ", ").getItem(10))\
-        .withColumn("tstamp", split(df["parsed_value"].cast("string"), ", ").getItem(11))
+df2 = df.withColumn("ax1", split(df["parsed_value"].cast("string"), ", ").getItem(0))\
+        .withColumn("ax2", split(df["parsed_value"].cast("string"), ", ").getItem(1).cast("double"))\
+        .withColumn("ax3", split(df["parsed_value"].cast("string"), ", ").getItem(2).cast("double"))\
+        .withColumn("ax4", split(df["parsed_value"].cast("string"), ", ").getItem(3).cast("double"))\
+        .withColumn("ax5", split(df["parsed_value"].cast("string"), ", ").getItem(4).cast("double"))\
+        .withColumn("ax6", split(df["parsed_value"].cast("string"), ", ").getItem(5).cast("double"))\
+        .withColumn("ax7", split(df["parsed_value"].cast("string"), ", ").getItem(6).cast("double"))\
+        .withColumn("ax8", split(df["parsed_value"].cast("string"), ", ").getItem(7).cast("double"))\
+        .withColumn("ax9", split(df["parsed_value"].cast("string"), ", ").getItem(8).cast("double"))\
+        .withColumn("ax10", split(df["parsed_value"].cast("string"), ", ").getItem(9).cast("double"))\
+        .withColumn("ax11", split(df["parsed_value"].cast("string"), ", ").getItem(10).cast("double"))\
+        .withColumn("ax12", split(df["parsed_value"].cast("string"), ", ").getItem(11).cast("double"))\
+        .withColumn("activity", split(df["parsed_value"].cast("string"), ", ").getItem(12))\
+        .withColumn("tstamp", split(df["parsed_value"].cast("string"), ", ").getItem(13))
 
-df2 = df2.withColumn("pts", split(F.col("tstamp"), "}").getItem(0))
-df2 = df2.withColumn("pts2", F.to_timestamp(F.col("pts"), "yyyy-MM-dd HH:mm:ss"))
+df2 = df2.withColumn("ax1-up", split(F.col("ax1"), "\{").getItem(1).cast("double"))
+drop_cols = ("parsed_value", "ax1")
+df2 = df2.drop(*drop_cols)
+df2 = df2.withColumnRenamed("ax1-up", "ax1")
 
-df2.writeStream.format("console").outputMode("update").start()
+df2.writeStream.format("console").outputMode("update").option("truncate", False).start()
